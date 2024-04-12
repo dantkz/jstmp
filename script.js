559976beb42b877e47b99ff88664548f4354b342
Daniyar Turmukhambetov <daniyar@nianticlabs.com>
2024-04-12 14:37:34 +0100

    unicorns

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-// Load sprite image
-const sprite = new Image();
-sprite.src = 'sprite.png'; // Replace 'sprite.png' with the path to your sprite image
+// Load sprite images
+const barbie = new Image();
+barbie.src = 'barbie.png';
+
+const unicorns = new Image();
+unicorns.src = 'unicorns.png';
 
 // Define sprite properties
 const spriteWidth = 200; // Width of each sprite frame
 const spriteHeight = 200; // Height of each sprite frame
-const numFrames = 1; // Number of frames in the sprite
+const numFrames = 2; // Number of frames in the sprite
 let currentFrame = 0; // Current frame to display
 
 // Function to draw sprite
-function drawSprite() {
+function drawSprite(target) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.drawImage(
-    sprite, 
-    currentFrame * spriteWidth, // X coordinate of the sprite frame
+    target, 
+    0, // X coordinate of the sprite frame
     0, // Y coordinate of the sprite frame (assuming it's on the top)
     spriteWidth, // Width of the sprite frame
     spriteHeight, // Height of the sprite frame
@@ -30,7 +33,11 @@ function drawSprite() {
 // Update animation frame
 function updateFrame() {
   currentFrame = (currentFrame + 1) % numFrames;
-  drawSprite();
+  if (currentFrame === 0){
+    drawSprite(barbie);
+  } else {
+    drawSprite(unicorns);
+  }
 }
 
 // Start animation
